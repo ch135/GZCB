112bf4682a1ac5366a16a286fe458b5d06cc5a1a
ch135 <707347865@qq.com>
2020-11-20 00:36:29 +0000

    新核心

---
+++ GZCB/src/main/java/com/work/demo/GZCB/NewCode.java
@@ -0,0 +1,36 @@
+package com.work.demo.GZCB;
+
+/** 
+* @author CHao
+* @version 2020年11月19日 下午11:10:09
+* <h3>新核心改造工具文件</h2>
+*/
+public class NewCode {
+	public static void main(String[] args) {
+		
+		System.out.println();
+	}
+	
+	/**
+	 * <h4>生成前端文件</h4>
+	 */
+	public static void getJSP() {
+		
+	}
+	
+	/**
+	 * <h4>生成报文体</h4>
+	 */
+	public static void getBody() {
+		
+	}
+	
+	/**
+	 * <h4>生成报文头</h4>
+	 */
+	public static void getHead() {
+		
+	}
+	
+}
+
